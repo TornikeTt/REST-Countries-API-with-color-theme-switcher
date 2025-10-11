798b51d5e1376cf03cb9fe8d8742a5efc9958f8d
Tornike <chargeishvilitornike@gmail.com>
2025-10-11 10:50:39 +0400

    Implement dark mode toggle and styling with Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import { useState } from "react";
+import Header from "./Components/Header/Header.jsx";
+
 import "./App.scss";
 
-function App() {}
+function App() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const themeStyles = {
+        bgColor: {
+            background: isDarkMode ? "hsl(207, 26%, 17%)" : "hsl(0, 0%, 99%)",
+        },
+
+        elementColor: {
+            background: isDarkMode
+                ? "hsl(209, 23%, 22%)"
+                : "hsl(0, 100%, 100%)",
+        },
+
+        textColor: {
+            color: isDarkMode ? "hsl(0, 100%, 100%)" : "hsl(200, 15%, 8%)",
+        },
+    };
+
+    return (
+        <div className="App" style={themeStyles.bgColor}>
+            <Header
+                isDarkMode={isDarkMode}
+                setIsDarkMode={setIsDarkMode}
+                themeStyles={themeStyles}
+            />
+        </div>
+    );
+}
 
 export default App;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import { FaRegMoon } from "react-icons/fa";
+import { FaMoon } from "react-icons/fa";
+import "./Header.scss";
+
+function Header({ isDarkMode, setIsDarkMode, themeStyles }) {
+    const toggleTheme = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    return (
+        <div className="header-container" style={themeStyles.elementColor}>
+            <div className="header">
+                <h1 style={themeStyles.textColor}>Where in the world?</h1>
+                <button onClick={toggleTheme}>
+                    {isDarkMode ? <FaMoon color="white" /> : <FaRegMoon />}
+                    <span style={themeStyles.textColor}>Dark Mode</span>
+                </button>
+            </div>
+        </div>
+    );
+}
+
+export default Header;
